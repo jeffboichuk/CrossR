95f94e291ee3d9b12c0c0ba7f542ec5f39bea01a
Jeff Boichuk <boichuk@virginia.edu>
2019-02-03 09:43:53 -0500

    Added "There you go again, being excellent."

--- R/get-encouragement.R
+++ R/get-encouragement.R
@@ -1,7 +1,8 @@
 encouragement <- c(
    "Encouragement!",
    "Look at you getting faster!",
-   "More fluent with R than you were when you woke up, I see. :)"
+   "More fluent with R than you were when you woke up, I see. :)",
+   "There you go again, being excellent."
 )
 
 get_encouragement <- function() {
